0c65eee02f8641bfeb5289144d25d98a76631adb
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-03-23 19:00:37 +0100

    Fixed error message.

--- utils/utils.go
+++ utils/utils.go
@@ -79,7 +79,7 @@ func SliceImage(fileOriginal string, outPutDir string, rows int, cols int, doneC
 	}
 
 	if imageFormat != "png" && imageFormat != "jpg" && imageFormat != "jpeg" {
-		errorChannel <- fmt.Errorf(fmt.Sprintln("Format has to be png or jpg."))
+		errorChannel <- fmt.Errorf(fmt.Sprintln("Format has to be png, jpg or jpeg."))
 		return
 	}
 
